e1c7d44b1013f7a3437835ad5f13ae12d13a4bfd
mindbreakers1792 <noreply>
2016-06-30 01:53:58 +0000

    Add component fer

---
+++ ui/fer.reel/fer.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/fer.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Fer
+ * @extends Component
+ */
+exports.Fer = Component.specialize(/** @lends Fer# */ {
+    constructor: {
+        value: function Fer() {
+            this.super();
+        }
+    }
+});
